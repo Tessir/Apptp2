385271921cd65c83cd5f61c53c9106bdd60baf5c
Tessir <80093094+Tessir@users.noreply.github.com>
2021-10-29 10:35:51 +0200

    versionfinal

--- activites/src/main/java/com/gmail/tfahmiali/activites/ComputeActivity.kt
+++ activites/src/main/java/com/gmail/tfahmiali/activites/ComputeActivity.kt
@@ -3,29 +3,43 @@ package com.gmail.tfahmiali.activites
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+// import android.widget.Button
+// import android.widget.EditText
+// import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+// import com.gmail.tfahmiali.activites.databinding.ActivityMainBinding//
+import com.gmail.tfahmiali.activites.databinding.ComputeActivityBinding
 
 class ComputeActivity : AppCompatActivity(), TextWatcher {
-    private lateinit var nombre1: EditText
-    private lateinit var nombre2: EditText
-    private lateinit var resultat: TextView
-    private lateinit var calculbutton: Button
+    private lateinit var binding: ComputeActivityBinding
+//    private lateinit var nombre1: EditText
+//    private lateinit var nombre2: EditText
+//    private lateinit var resultat: TextView
+//    private lateinit var calculbutton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.compute_activity)
-        nombre1 = findViewById(R.id.field_1)
-        nombre2 = findViewById(R.id.field_2)
-        resultat = findViewById(R.id.resultat)
-        calculbutton = findViewById(R.id.btn_calculer)
-        nombre1.addTextChangedListener(this)
-        nombre2.addTextChangedListener(this)
-        calculbutton.setOnClickListener {
+//        setContentView(R.layout.compute_activity)
+        binding = ComputeActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        nombre1 = findViewById(R.id.field_1)
+//        nombre2 = findViewById(R.id.field_2)
+//        resultat = findViewById(R.id.resultat)
+//        calculbutton = findViewById(R.id.btn_calculer)
+//        nombre1.addTextChangedListener(this)
+//        nombre2.addTextChangedListener(this)
+        binding.field1.addTextChangedListener(this)
+        binding.field2.addTextChangedListener(this)
+//        calculbutton.setOnClickListener {
+//            val resultatcalcul =
+//                nombre1.text.toString().toDouble() + nombre2.text.toString().toDouble()
+//            resultat.text = resultatcalcul.toString()
+//        }
+        binding.btnCalculer.setOnClickListener {
             val resultatcalcul =
-                nombre1.text.toString().toDouble() + nombre2.text.toString().toDouble()
-            resultat.text = resultatcalcul.toString()
+                binding.field1.text.toString().toDouble() + binding.field2.text.toString()
+                    .toDouble()
+            binding.resultat.text = resultatcalcul.toString()
         }
     }
 
@@ -36,6 +50,8 @@ class ComputeActivity : AppCompatActivity(), TextWatcher {
     }
 
     override fun afterTextChanged(s: Editable?) {
-        calculbutton.isEnabled = nombre1.text.isNotBlank() && nombre2.text.isNotBlank()
+//        calculbutton.isEnabled = nombre1.text.isNotBlank() && nombre2.text.isNotBlank()
+        binding.btnCalculer.isEnabled =
+            binding.field1.text.isNotBlank() && binding.field2.text.isNotBlank()
     }
 }
--- activites/src/main/java/com/gmail/tfahmiali/activites/MainActivity.kt
+++ activites/src/main/java/com/gmail/tfahmiali/activites/MainActivity.kt
@@ -2,36 +2,41 @@ package com.gmail.tfahmiali.activites
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+// import android.widget.Button
+// import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.gmail.tfahmiali.activites.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var clickButton: Button
-    private lateinit var compteur: TextView
-    private lateinit var computeButton: Button
+//    private lateinit var clickButton: Button
+//    private lateinit var compteur: TextView
+//    private lateinit var computeButton: Button
+    private lateinit var binding: ActivityMainBinding
     private var nbClick = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        compteur = findViewById(R.id.nbr_de_fois)
-        clickButton = findViewById(R.id.btn_click_me)
-        computeButton = findViewById(R.id.btn_compute)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        setContentView(R.layout.activity_main)
+//        compteur = findViewById(R.id.nbr_de_fois)
+//        clickButton = findViewById(R.id.btn_click_me)
+//        computeButton = findViewById(R.id.btn_compute)
 //        clickButton.setOnClickListener {
 //            Toast.makeText(baseContext, "Tu m'as cliqué", Toast.LENGTH_LONG).show()
 //        }
-        computeButton.setOnClickListener {
+        binding.btnCompute.setOnClickListener {
             val intent = Intent(baseContext, ComputeActivity::class.java)
             startActivity(intent)
         }
-        clickButton.setOnClickListener {
+        binding.btnClickMe.setOnClickListener {
             nbClick++
             if (nbClick != 0) {
                 val newText = "Vous avez cliquez $nbClick fois"
-                compteur.text = newText
+                binding.nbrDeFois.text = newText
             }
             if (nbClick == 5) {
-                clickButton.isEnabled = false
+//                clickButton.isEnabled = false
+                binding.btnClickMe.isEnabled = false
             }
         }
     }
